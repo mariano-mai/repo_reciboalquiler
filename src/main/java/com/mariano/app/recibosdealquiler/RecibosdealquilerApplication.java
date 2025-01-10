8d421474231be1906ac2ef9c3807090ffaa69b6e
Mariano <mariano.ramaidana@gmail.com>
2025-01-10 20:30:22 -0300

    primer commit, inicio del proyecto

---
+++ src/main/java/com/mariano/app/recibosdealquiler/RecibosdealquilerApplication.java
@@ -0,0 +1,13 @@
+package com.mariano.app.recibosdealquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecibosdealquilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecibosdealquilerApplication.class, args);
+	}
+
+}
